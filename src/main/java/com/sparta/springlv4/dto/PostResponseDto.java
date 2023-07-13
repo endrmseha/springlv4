7a9dd72dc438c8c5a76478a5637f2fb265ae20d6
endrmseha <endrmseha@naver.com>
2023-07-14 08:43:18 +0900

    댓글, 좋아요 기능 완료

--- src/main/java/com/sparta/springlv4/dto/PostResponseDto.java
+++ src/main/java/com/sparta/springlv4/dto/PostResponseDto.java
@@ -8,25 +8,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
+
 public class PostResponseDto {
-    private long id;
+    private Long id;
+    private String username;
     private String title;
     private String contents;
-    private String username;
     private LocalDateTime createAt;
     private LocalDateTime modifiedAt;
     private List<CommentResponseDto> commentList;
-    // password는 보여주지 않도록 함
 
-
-    public PostResponseDto(Post post) {
+    public PostResponseDto(Post post){
         this.id = post.getId();
+        this.username = post.getUser().getUsername();
         this.title = post.getTitle();
         this.contents = post.getContents();
-        this.username = post.getUsername();
         this.createAt = post.getCreatedAt();
         this.modifiedAt = post.getModifiedAt();
         this.commentList = post.getCommentList().stream()
                 .map(CommentResponseDto::new).collect(Collectors.toList());
     }
+
 }
